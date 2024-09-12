6f663da9c9c7063af1f5fe800b0ca63dfcbea289
vdynak <victoria.dynak.801839@gmail.com>
2024-09-12 17:33:24 -0400

    Initial Commit

---
+++ HIIT/HIITApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITApp.swift
+//  HIIT
+//
+//  Created by Victoria Dynak on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct HIITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
